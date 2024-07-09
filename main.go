e1a2e9e1c17fa5fa2d49eb3cd0de03346aea7e41
pgrossma <pgrossma@student.42heilbronn.de>
2024-07-09 15:32:29 +0000

    tried the one mainModal stragegy

--- main.go
+++ main.go
@@ -1,70 +1,15 @@
 package main
 
 import (
+	"core-cli/models"
 	"fmt"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/huh"
 )
 
-type Model struct {
-    form *huh.Form
-}
-
-func NewModel() Model {
-	return Model{
-        form: huh.NewForm(
-            huh.NewGroup(
-				huh.NewSelect[string]().
-					Key("class").
-					Options(huh.NewOptions("Warrior", "Mage", "Rogue")...).
-					Title("Choose your class"),
-
-            huh.NewSelect[int]().
-                Key("level").
-                Options(huh.NewOptions(1, 20, 9999)...).
-                Title("Choose your level"),
-            ),
-        ),
-    }
-}
-
-func (m Model) Init() tea.Cmd {
-	return m.form.Init()
-}
-
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "esc", "ctrl+c", "q":
-			return m, tea.Quit
-		}
-	}
-
-	var cmds []tea.Cmd
-	// Process the form
-	form, cmd := m.form.Update(msg)
-	if f, ok := form.(*huh.Form); ok {
-		m.form = f
-		cmds = append(cmds, cmd)
-	}
-
-	return m, tea.Batch(cmds...)
-}
-
-func (m Model) View() string {
-	if m.form.State == huh.StateCompleted {
-        class := m.form.GetString("class")
-        level := m.form.GetInt("level")
-        return fmt.Sprintf("You selected: %s, Lvl. %d", class, level)
-    }
-    return m.form.View()
-}
-
 func main() {
-	_, err := tea.NewProgram(NewModel()).Run()
+	_, err := tea.NewProgram(models.NewMainModel()).Run()
 	if err != nil {
 		fmt.Println("Oh no:", err)
 		os.Exit(1)
