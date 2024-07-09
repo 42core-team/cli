4c65b99a546bb417c950656dbdb2ba41c87117de
Jonas Götz <jonas.goetz01@web.de>
2024-07-09 09:27:19 +0000

    Working select

--- cli/menu.go
+++ cli/menu.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -24,8 +25,8 @@ func NewModel() Model {
 	m.form = huh.NewForm(
 		huh.NewGroup(
 			huh.NewSelect[string]().
-				Key("class").
-				Options(huh.NewOptions("Manage Teams", "Mage", "Rogue")...).
+				Key("option").
+				Options(huh.NewOptions("Manage Teams", "Tournament", "Leaderboard")...).
 				Title("Main menu").
 				Description("Choose what you want to do").Value(&m.chosen),
 		),
@@ -56,10 +57,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmds = append(cmds, cmd)
 	}
 
-	if m.form.State == huh.StateCompleted {
-
-		return NewPage2Model(), tea.Batch(cmds...)
-	}
 	return m, tea.Batch(cmds...)
 }
 
@@ -70,6 +67,20 @@ func (m Model) View() string {
 	form := m.lg.NewStyle().Margin(1, 0).Render(v)
 
 	body := lipgloss.JoinHorizontal(lipgloss.Top, form)
+
+	if m.form.State == huh.StateCompleted {
+		fmt.Println(m.form.Get("option"))
+		switch m.form.Get("option") {
+		case "Manage Teams":
+			return s.Base.Render("Manage Teams")
+		case "Tournament":
+			return s.Base.Render("Tournament")
+		case "Leaderboard":
+			return s.Base.Render("Leaderboard")
+		default:
+			fmt.Println("Go fuck yourself")
+		}
+	}
 
 	return s.Base.Render(body)
 
