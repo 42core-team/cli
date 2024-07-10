48878d6227992d3cfb905b4231b0b812502444e6
pgrossma <pgrossma@student.42heilbronn.de>
2024-07-10 13:24:41 +0000

    back navigation works

---
+++ tui/utils.go
@@ -0,0 +1,15 @@
+package tui
+
+import tea "github.com/charmbracelet/bubbletea"
+
+func switchState(m *Model, state State) (tea.Model, tea.Cmd) {
+	m.state = state
+	switch state {
+	case TListState:
+		return *m, initTListForm(m)
+	case TDetailsState:
+		return *m, initTDetailsForm(m)
+	default:
+		return *m, nil
+	}
+}
