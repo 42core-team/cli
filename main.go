12544b6d2419f579dc9922afedf5bffe3ca10ed2
Paulic <61714149+PaulicStudios@users.noreply.github.com>
2024-08-09 15:39:11 +0000

    first working with with db

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"core-cli/db"
 	"core-cli/github"
+	"core-cli/tui"
 	"log"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/joho/godotenv"
 )
 
@@ -16,8 +18,9 @@ func main() {
 		log.Fatalln("Error creating GitHub client:", err)
 	}
 	db.Connect()
-	// _, err = tea.NewProgram(tui.NewModel()).Run()
-	// if err != nil {
-	// 	log.Fatalln("Error running program:", err)
-	// }
+
+	_, err = tea.NewProgram(tui.NewModel()).Run()
+	if err != nil {
+		log.Fatalln("Error running program:", err)
+	}
 }
