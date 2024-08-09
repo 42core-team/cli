2e2a734c9c9684be1e7bbf2f6911cb92dc7e58de
Paulic <61714149+PaulicStudios@users.noreply.github.com>
2024-08-09 14:57:01 +0000

    added CRUD operation for db

---
+++ db/get.go
@@ -0,0 +1,39 @@
+package db
+
+import "core-cli/model"
+
+func GetPlayerByIntraName(intraName string) *model.Player {
+	var player model.Player
+	db.Where("intra_name = ?", intraName).First(&player)
+	return &player
+}
+
+func GetPlayerByGithubName(githubName string) *model.Player {
+	var player model.Player
+	db.Where("github_name = ?", githubName).First(&player)
+	return &player
+}
+
+func GetTeamByName(name string) *model.Team {
+	var team model.Team
+	db.Where("name = ?", name).First(&team)
+	return &team
+}
+
+func GetTeamByRepoName(repoName string) *model.Team {
+	var team model.Team
+	db.Where("repo_name = ?", repoName).First(&team)
+	return &team
+}
+
+func GetPlayersByTeamID(teamID uint) []model.Player {
+	var players []model.Player
+	db.Where("team_id = ?", teamID).Find(&players)
+	return players
+}
+
+func GetTeams() []model.Team {
+	var teams []model.Team
+	db.Find(&teams)
+	return teams
+}
