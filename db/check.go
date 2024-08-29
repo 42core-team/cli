70b2ba6450252605991dc930495a2cd52fac62b5
pgrossma <pgrossma@student.42heilbronn.de>
2024-08-29 16:41:10 +0200

    modified db cheks for ignorecase

--- db/check.go
+++ db/check.go
@@ -4,18 +4,18 @@ import "core-cli/model"
 
 func PlayerExistsByIntraName(intraName string) bool {
 	var count int64
-	db.Model(&model.Player{}).Where("intra_name = ?", intraName).Count(&count)
+	db.Model(&model.Player{}).Where("LOWER(intra_name) = LOWER(?)", intraName).Count(&count)
 	return count > 0
 }
 
 func PlayerExistsByGithubName(githubName string) bool {
 	var count int64
-	db.Model(&model.Player{}).Where("github_name = ?", githubName).Count(&count)
+	db.Model(&model.Player{}).Where("LOWER(github_name) = LOWER(?)", githubName).Count(&count)
 	return count > 0
 }
 
 func TeamExistsByName(name string) bool {
 	var count int64
-	db.Model(&model.Team{}).Where("name = ?", name).Count(&count)
+	db.Model(&model.Team{}).Where("LOWER(name) = LOWER(?)", name).Count(&count)
 	return count > 0
 }
