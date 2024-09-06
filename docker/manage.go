e0868814db023c8ece6057088531be17b16ae5bb
pgrossma <pgrossma@student.42heilbronn.de>
2024-09-06 15:09:35 +0000

    added more docker manage functions

--- docker/manage.go
+++ docker/manage.go
@@ -6,22 +6,35 @@ import (
 	"github.com/docker/docker/api/types/container"
 )
 
-func RunImage(image string) error {
+func RunImage(image string) (*string, error) {
 	err := PullImage(image)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	resp, err := CreateContainer(image)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.ID, StartContainer(resp.ID)
+}
+
+func CreateContainer(image string) (container.CreateResponse, error) {
 	resp, err := cli.ContainerCreate(context.Background(), &container.Config{
 		Image: image,
 	}, nil, nil, nil, "")
-	if err != nil {
-		return err
-	}
+	return resp, err
+}
 
-	return cli.ContainerStart(context.Background(), resp.ID, container.StartOptions{})
+func StartContainer(id string) error {
+	return cli.ContainerStart(context.Background(), id, container.StartOptions{})
 }
 
 func StopContainer(id string) error {
 	return cli.ContainerStop(context.Background(), id, container.StopOptions{})
+}
+
+func RemoveContainer(id string) error {
+	return cli.ContainerRemove(context.Background(), id, container.RemoveOptions{})
 }
