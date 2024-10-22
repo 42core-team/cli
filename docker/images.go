0abc26d746034e5c38ab68a67e18dd4078f168a1
Paul Großmann <git@paulgrossmann.de>
2024-10-22 15:51:14 +0000

    fix image pulling

--- docker/images.go
+++ docker/images.go
@@ -2,12 +2,13 @@ package docker
 
 import (
 	"context"
+	"io"
 
 	"github.com/docker/docker/api/types/image"
 )
 
 func PullImage(imageUrl string) error {
-	_, err := cli.ImagePull(context.Background(), imageUrl, image.PullOptions{})
-	// io.Copy(os.Stdout, reader)
+	reader, err := cli.ImagePull(context.Background(), imageUrl, image.PullOptions{})
+	io.Copy(io.Discard, reader)
 	return err
 }
